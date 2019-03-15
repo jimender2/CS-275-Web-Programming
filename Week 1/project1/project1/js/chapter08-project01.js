ac5caf6c65994ea6d1d5635dc104b3b75090bea8
jimender2 <35303639+jimender2@users.noreply.github.com>
2019-03-14 23:54:53 -0400

    Finished with project1 in Week 1

--- Week 1/project1/project1/js/chapter08-project01.js
+++ Week 1/project1/project1/js/chapter08-project01.js
@@ -1,3 +1,25 @@
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
 
+
+(function () {
+	"use strict";
+	var i = 0;
+	var total = 0;
+	while (i < filenames.length) {
+		outputCartRow( filenames[i], titles[i], quantities[i], prices[i].toFixed(2), calculateTotal(quantities[i], prices[i]).toFixed(2));
+		total = total + calculateTotal(quantities[i], prices[i]);
+		i++;
+	}
+	var tax = (total*0.10);
+	calcRow( "Subtotal", total.toFixed(2), false);
+	calcRow( "Tax", tax.toFixed(2), false);
+	
+	var shippingCost = 40;
+	if (total >= 1000){
+		shippingCost = 0;
+	}
+	calcRow( "Shipping", shippingCost, false);
+	calcRow( "Grand Total", (total + tax + shippingCost).toFixed(2), true);
+
+})();
--- Week 1/project1/project1/js/functions.js
+++ Week 1/project1/project1/js/functions.js
@@ -1,4 +1,26 @@
 /* define functions here */
 
-
+function calculateTotal( quanity, price) {
+	return quanity * price;
+}
         
+function outputCartRow( file, title, quantity, price, total){
+	document.write("<tr>");
+    document.write("<td><img src='images/" + file + "'></td>");
+    document.write("<td>" + title + "</td>");
+    document.write("<td>" + quantity + "</td>");
+    document.write("<td>$" + price + "</td>");
+    document.write("<td>$" + total + "</td>");
+    document.write("</tr>");
+}
+
+function calcRow( name, amount, bold){
+	if (!bold){
+		document.write("<tr class=\"totals\">");
+	} else{
+		document.write("<tr class=\"totals focus\">");
+   	}
+    document.write("<td colspan=\"4\">" + name + "</td>");
+    document.write("<td>$" + amount + "</td>");
+    document.write("</tr>");
+}
